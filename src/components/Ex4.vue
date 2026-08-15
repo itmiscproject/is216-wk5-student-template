8e68af3df0c9d1e19a6a3b156f0c60d497ec79e7
cookiemo729 <58022041+cookiemo729@users.noreply.github.com>
2026-08-15 10:51:31 +0800

    Changed Ex4

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -1,21 +1,16 @@
 <script setup>
 import { ref } from 'vue'
 const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
-const newItem = ref('')
 
-function add() {
-    items.value.push(newItem.value)
-    newItem.value = ''
-}   
+// TODO: Add Code Here
+
+
 </script>
 
 <template>
+
     <h2>Shopping Cart</h2>
-    <ul>
-        <li v-for="(item, i) in items">
-            {{item}}
-        <button @click="items.splice(i, 1)">Delete!</button> </li>
-    </ul>
-    <input @keyup.enter="add" v-model="newItem"/>
-    <button @click="add">Add!</button>    
+    <!-- TODO: Add Code Here -->
+
+   
 </template>
