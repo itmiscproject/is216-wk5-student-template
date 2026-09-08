561cce9c456f5127bf1fe54e8f66d604e210c3c0
cookiemo729 <58022041+cookiemo729@users.noreply.github.com>
2026-09-09 02:37:02 +0800

    Using class

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -2,29 +2,45 @@
 
 import { ref } from 'vue';
 
-const id = ref('circle')
+const cls = ref('circle')
 
 
 function mOver() {
     // add code here
-    id.value = "square"
+    cls.value = "square"
 }
 
 function mOut() {
     // add code here
-    id.value = "circle"
+    cls.value = "circle"
 }
 </script>
 
 <template>
     <!-- modify code below -->
-    <div :id="id" @mouseover="mOver" @mouseout="mOut">
+    <div id="shape" :class="cls" @mouseover="mOver" @mouseout="mOut">
         Mouse Over Me
     </div>
 
 </template>
 
 <style scoped>
+#shape {
+    width: 150px;
+    height: 150px;
+    background-color: goldenrod;
+    text-align: center;
+    padding-top: 60px;
+}
+
+.circle {
+    border-radius: 50%;
+}
+
+.square {
+    border-radius: 0%;
+}
+/*
 #square {
     width: 150px;
     height: 150px;
@@ -41,4 +57,5 @@ function mOut() {
     text-align: center;
     padding-top: 60px;
 }
+*/
 </style>
